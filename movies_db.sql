55355a1ee5f6ae4828fdd4d36f38a7bb3efde0bf
Karra Avinash <2000170061@hexaware.com>
2026-08-18 14:33:18 +0530

    Added Movies DB with few Table Definitions

---
+++ movies_db.sql
@@ -0,0 +1,39 @@
+-- Database: movies_db
+
+-- DROP DATABASE IF EXISTS movies_db;
+
+CREATE DATABASE movies_db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_India.1252'
+    LC_CTYPE = 'English_India.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
+	
+	-- Directors Table 
+	CREATE TABLE directors(
+		director_id SERIAL Primary Key,
+		fName varchar(30),
+		lName varchar(30) not null,
+		dob date,
+		nationality varchar(20)
+	);
+	
+	SELECT * From directors;
+	
+	-- DROP TABLE directors;
+	
+	-- Actors Table without Primary Key
+	CREATE TABLE actors(
+		actor_id serial,
+		fName varchar(30),
+		lName varchar(30) not null,
+		gender char(1),
+		dob date
+	);
+	
+	SELECT * from actors;
