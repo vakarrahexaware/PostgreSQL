f1c7e3cedda25a6759a90c1ddab576cd78f6b0e3
Karra Avinash <2000170061@hexaware.com>
2026-08-18 14:47:55 +0530

    Added a junction Table

--- movies_db.sql
+++ movies_db.sql
@@ -36,4 +36,42 @@ CREATE DATABASE movies_db
 		dob date
 	);
 	
+	ALTER TABLE actors
+	ADD CONSTRAINT actor_pkey PRIMARY KEY(actor_id)
+	
 	SELECT * from actors;
+	
+	
+	-- Create Movies Table with FK of Directors
+	CREATE TABLE movies(
+		movie_id serial Primary Key,
+		movie_name varchar(50),
+		movie_length int,
+		movie_lang varchar(20),
+		release_date date,
+		age_certificate varchar(5),
+		director_id int references directors(director_id)
+	);
+	
+	SELECT * FROM movies;
+	
+	-- Create Movie Revenues Table with movie_id as FK
+	CREATE TABLE movie_revenues(
+		revenue_id serial Primary Key,
+		movie_id int references movies(movie_id),
+		domestic_takings numeric(6,2) not null,
+		international_takings numeric(6,2)
+	);
+	
+	SELECT * FROM movie_revenues;
+	
+	-- Create a JUNCTION TABLE for movie and actors
+	CREATE TABLE movie_actors(
+		mov_id INT references movies(movie_id),
+		act_id INT references actors(actor_id),
+		Primary Key(mov_id, act_id)
+	);
+	
+	SELECT * FROM movie_actors;
+	
+	SELECT * FROM movie_actors;
