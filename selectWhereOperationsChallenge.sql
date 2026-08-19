cc00a328255f550792c16c472703dfc08a699ec1
--replace-all <karraa@hexaware.com>
2026-08-19 17:20:40 +0530

    challenge questions are added

---
+++ selectWhereOperationsChallenge.sql
@@ -0,0 +1,10 @@
+--  Select everything from Movie
+SELECT * FROM movies;
+
+-- Select movie_name and release_date for every movie
+
+-- Select First and Last names for American Directors
+
+-- Select all male actors born after the 1st of January 1970
+
+-- Select the names of all movies which over 90 mins long and movie language is English
