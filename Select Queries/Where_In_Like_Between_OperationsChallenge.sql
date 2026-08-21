2c3504d963f24707c0361e646fa9319e4a01b0fc
--replace-all <karraa@hexaware.com>
2026-08-21 11:40:03 +0530

    Naming changes

--- Select Queries/Where_In_Like_Between_OperationsChallenge.sql
+++ Select Queries/Where_In_Like_Between_OperationsChallenge.sql
@@ -20,7 +20,8 @@ WHERE gender = 'M' AND dob > '1970-01-01';
 
 -- Select the names of all movies which over 90 mins long and movie language is English
 SELECT movie_name FROM movies
-WHERE movie_length > 90 AND movie_lang = 'English';
+WHERE movie_length > 90 
+AND movie_lang = 'English';
 
 -- Select movie name and movie languages of all movies with a movie language of english, spanish or Korean
 SELECT movie_name, movie_lang FROM movies
@@ -28,9 +29,11 @@ WHERE movie_lang IN ('English', 'Spanish', 'Korean');
 
 -- Select fName and lName of the actors whose last Name begins with m and were born between 01/01/1940 and 31/12/1969
 SELECT fname, lname FROM actors
-WHERE lname LIKE 'M%' AND dob BETWEEN '1940-01-01' AND '1969-12-31';
+WHERE lname LIKE 'M%' 
+AND dob BETWEEN '1940-01-01' AND '1969-12-31';
 
 -- Select the first and last names of the directors with 
 -- nationality of British, French or German born between 01/01/1950 and 31/12/1980
 SELECT first_name, last_name FROM directors
-WHERE nationality IN ('British', 'French', 'German') AND dob BETWEEN '1950-01-01' AND '1980-12-31'; 
+WHERE nationality IN ('British', 'French', 'German') 
+AND dob BETWEEN '1950-01-01' AND '1980-12-31'; 
