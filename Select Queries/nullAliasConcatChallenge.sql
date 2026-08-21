04bb440651ccba0b160cf4dffd9543d9a241aa11
--replace-all <karraa@hexaware.com>
2026-08-21 11:22:14 +0530

    Concat CHallenge questions are added

---
+++ Select Queries/nullAliasConcatChallenge.sql
@@ -0,0 +1,7 @@
+SELECT * FROM movies;
+
+-- Retrun  top 3 movies with the highest International Takings
+
+-- Concat first and last names of directors, seperated by space and call this new column full_name
+
+-- Return the actors with missing first_name or missing date_of_births
